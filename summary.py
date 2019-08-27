b2ac5342aa5dd497090b3811f01c4b1cc831c2e7
ricksonre <ricksonre@gmail.com>
2019-08-27 11:47:45 -0700

    0.3

--- summary.py
+++ summary.py
@@ -14,19 +14,30 @@ class Controller:
     """ 
     def __init__(self):
         self.months = []
+        self.pricePerMonth = []
         self.size  = 0
         self.numMonths = 0
+        self.totalPrice = Decimal(0)
 
     def printPurchase(self, aux):
         print("{}, {}, {}".format(time.strftime("%m/%d/%Y", aux[0]),
                               aux[1], str(aux[2])))
 
+    def calculatePricePerMonth(self):
+        for month in self.months:
+            aux = Decimal(0)
+            for p in month:
+                aux +=p[2]
+            self.pricePerMonth.append(aux)
+                
+
     #reads the the file of the given fileName
     def readFile(self, fileName):
         #list to read all purchases into
         auxList = []
-
+       
         try:
+            
             with open(fileName) as file:
                 reader = csv.reader(file)
                 for row in reader:
@@ -34,40 +45,49 @@ class Controller:
                     date = time.strptime(row[0], "%m/%d/%Y")
                     #saves the price as a decimal
                     value = Decimal(sub(r'[^\d\-.]', '', row[2]))
+                    #increase the total price spent
+                    self.totalPrice += value
                     #appends it in the temp list
                     auxList.append((date, row[1], value))
-                
+               
                 #sorts the list based on the daate
                 auxList.sort(key = lambda element: element[0])
                 #sets the size of the total number of purchases
                 self.size = len(auxList)
-
+                
                 #gets the number of the first month
                 month = auxList[0][0].tm_mon
-                index = 0
+                begin = 0
                 #add all purchases into the months list 
                 # with each months having its own index
-                for e in auxList:
-                    if(e[0].tm_mon != month): 
-                        index +=1
-                    self.months[index].append(e)
+                for i in range(0,len(auxList)):
+                    if(auxList[i][0].tm_mon != month):
+                        self.months.append(auxList[begin: i])
+                        month = auxList[i][0].tm_mon
+                        begin = i+1
+                        #print("Month: {}  begin: {}".format(
+                        #    auxList[i][0].tm_mon, begin))
+
                 #set the total number of months
                 self.numMonths = len(self.months)
-        except:
+                self.calculatePricePerMonth()
+
+            return True
+        except FileNotFoundError:
             print("File not found")
+            return False
 
     
     #searches for the index of a date
     # or if not found the next index
     def binarySearch(self, element):
-        
         mIndex = 0
         pIndex = 0
 
         low = 0
         high = self.numMonths - 1
         #binary search to find the index of the month
-        while(low < high):
+        while(low <= high):
             middle = int((high + low)/2)
             current = self.months[middle][0][0]
             if(current.tm_mon < element.tm_mon or current.tm_year < element.tm_year):
@@ -78,11 +98,11 @@ class Controller:
                 mIndex = middle
                 break
             mIndex = middle
-
+            
         low = 0
         high = len(self.months[mIndex]) -1
         #binary search to find the first purchase of the day
-        while(low < high):
+        while(low <= high):
             middle = int((high + low)/2)
             current = self.months[mIndex][middle][0]
             if(current < element):
@@ -96,40 +116,59 @@ class Controller:
                     middle -= 1
                 pIndex = middle
                 break
-            pIndex = middle
-
-        return (mIndex,pIndex)
+            pIndex = middle + 1
+            
+        
+        return [mIndex,pIndex]
 
     def printRange(self, begin, end):
         aux = self.months
         #out of range
-        if(aux[0][0] > end or aux[self.size-1][0] < begin):
+        if(aux[0][0][0] > end or aux[self.numMonths-1][len(aux[self.numMonths-1])-1][0] < begin):
             return
+        
+        temp = self.binarySearch(begin)
+        m = temp[0]
+        d = temp[1]
+        print(" {} {}".format(m,d))
+        while(aux[m][d][0] <= end):
+            self.printPurchase(aux[m][d])
+            if(d + 1 >= len(aux[m])):
+                m += 1
+                d = 0
+            else:
+                d += 1
 
-        index = self.binarySearch(begin)
-        while(aux[index][0] <= end):
-            self.printPurchase(aux[index])
-            index += 1
 """
     def printLastMonth(self):
         aux = self.months
         index  = self.binarySearch(time.struct_time(tm_mon = aux[self.size-1][0][1]))
 
-        for i in range(index, self.size-1):
+        for i in range(index, self.size):
             self.printPurchase(aux[i])
    """     
 
 
 def main():
-    fileName = "2019.csv"
+    fileName = "C:/Users/ricksonre/Documents/GitHub/accounting/2019.csv"
 
     cont = Controller()
-    cont.readFile(fileName)
+    while(not cont.readFile(fileName)):
+        break
+    
+    print(cont.totalPrice)
 
-    print(cont.months)
+    p = Decimal(0)
+    for pp in cont.pricePerMonth:
+        p+=pp
+    print(p)
+    #for i,month in enumerate(cont.months):
+    #    print(i)
+    #    for p in month:
+    #        cont.printPurchase(p)
 
-    #cont.printRange(time.strptime("03/03/2019", "%m/%d/%Y"),
-    #               time.strptime("05/03/2019", "%m/%d/%Y"))
+    #cont.printRange(time.strptime("03/01/2019", "%m/%d/%Y"),
+    #           time.strptime("05/03/2019", "%m/%d/%Y"))
 
     #cont.printLastMonth()    
 
